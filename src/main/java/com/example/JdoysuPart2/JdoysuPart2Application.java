4bbcf03a91dd8dd607fc74e10a023b83a1fe73e4
Jamire44 <2003doyle@gmail.com>
2024-03-03 21:48:26 +0000

    initial commit

---
+++ src/main/java/com/example/JdoysuPart2/JdoysuPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.JdoysuPart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdoysuPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdoysuPart2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JdoysuPart2/JdoysuPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JdoysuPart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdoysuPart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
